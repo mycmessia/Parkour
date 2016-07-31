7e92c947bc03a8e9acf607d39dc3d7478d57d858
mycmessia <164810193@qq.com>
2016-07-31 17:58:17 +0100

    Add change default FPS in IOS devices.

---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPS : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		Application.targetFrameRate = 60;
+	}
+}
